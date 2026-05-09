837617991e70bc1aaafb770ed719d8a4c2ef3883
Kai Zheng <144567523+kaizheng28@users.noreply.github.com>
2026-05-09 12:57:45 -0500

    udpated news and publications

---
+++ publications.js
@@ -0,0 +1,65 @@
+const PUBLICATIONS = [
+    `<li> <b>Physically Accurate Differentiable Inverse Rendering for Radio Frequency Digital Twin</b> <br>
+        Xingyu Chen, Xinyu Zhang, <b>Kai Zheng</b>, Xinmin Fang, Tzu-Mao Li, Chris Xiaoxuan Lu, and Zhengxiong Li. <br>
+        The ACM International Conference on Mobile Computing and Networking (<b>MobiCom</b>). 2026. <br>
+        (Acceptance Rate: 11.2%, 29 of 260).<br>
+    </li>`,
+    `<li> <b>MetaPolar: A Low-Cost, Passive, mmWave Radar-Readable Metasurface Road Sign</b> <br>
+        <b>Kai Zheng</b>, Wuqiong Zhao, and Xinyu Zhang. <br>
+        The International Conference on Embedded Artificial Intelligence and Sensing Systems (<b>SenSys</b>). 2026. <br>
+        (Acceptance Rate: 19.0%, 49 of 258).<br>
+        <a href="/paper/sensys26-MetaPolar.pdf">[PDF]</a>.
+    </li>`,
+    `<li> <b>NEAR: Neural Electromagnetic Array Response</b> <br>
+        Yinyan Bu, Jiajie Yu, <b>Kai Zheng</b>, Xinyu Zhang, and Piya Pal. <br>
+        Forty-Second International Conference on Machine Learning (<b>ICML</b>). 2025. <br>
+        (Acceptance Rate: 26.9%, 3,260 of 12,107).<br>
+        <a href="/paper/icml25-near.pdf">[PDF]</a>.
+    </li>`,
+    `<li> <b>Enhancing mmWave Radar Sensing Using a Phased-MIMO Architecture</b> <br>
+        <b>Kai Zheng</b>, Wuqiong Zhao, Timothy Woodford, Renjie Zhao, Xinyu Zhang, and Yingbo Hua. <br>
+        The 22nd Annual International Conference on Mobile Systems, Applications and Services (<b>MobiSys</b>). 2024. <br>
+        (Acceptance Rate: 16.3%, 43 of 263).<br>
+        <a href="/paper/mobisys24-hybRadar.pdf">[PDF]</a>.
+        <a href="/slides/Mobisys24_HybRadar_15-17min.pptx">[PPTX]</a>.
+    </li>`,
+    `<li> <b>NeuroRadar: A Neuromorphic Radar Sensor for Low-Power IoT Systems</b> (<font color="red">Best Paper Award</font>)<br>
+        <b>Kai Zheng</b>, Kun Qian, Timothy Woodford, and Xinyu Zhang. <br>
+        The 21th ACM Conference on Embedded Networked Sensor Systems (<b>SenSys</b>). 2023. <br>
+        (Acceptance Rate: 19%, 34 of 179).<br>
+        <a href="/paper/sensys23-NeuroRadar.pdf">[PDF]</a>.
+        <a href="/slides/Sensys23_NeuroRadar_14+3min.pptx">[PPTX]</a>.
+    </li>`,
+    `<li> <b>UniScatter: a Metamaterial Backscatter Tag for Wideband Joint Communication and Radar Sensing</b><br>
+        Kun Qian, Lulu Yao, <b>Kai Zheng</b>, Xinyu Zhang, and Tse Nga Ng. <br>
+        The 29th Annual International Conference On Mobile Computing And Networking (<b>MobiCom</b>). 2023. <br>
+        (Acceptance Rate: 29.4%, 40 of 136). <br>
+        <a href="/paper/mobicom23-uniscatter.pdf">[PDF]</a>.
+    </li>`,
+    `<li> <b>SlimWiFi: Ultra-Low-Power IoT Radio Architecture Enabled by Asymmetric Communication</b><br>
+        Renjie Zhao, Kejia Wang, <b>Kai Zheng</b>, Xinyu Zhang, Vincent Leung. <br>
+        The 20th USENIX Symposium on Networked Systems Design and Implementation (<b>NSDI</b>). 2023. <br>
+        (Acceptance Rate: 16.0%, 46 of 288). <br>
+        <a href="/paper/nsdi23-slimWiFi.pdf">[PDF]</a>.
+    </li>`,
+    `<li> <b>Calibrating a 4-channel fully-digital 60 GHz SDR</b> <br>
+        Aditya Dhananjay, <b>Kai Zheng</b>, Jaakko Haarla, Lorenzo Iotti, Marco Mezzavilla, Dennis Shasha, Sundeep Rangan. <br>
+        Proceedings of the 14th International Workshop on Wireless Network Testbeds, Experimental evaluation &amp; Characterization. (<b>WiNTECH</b>). 2020. <br>
+        <a href="/paper/dhananjay2020calibrating.pdf">[PDF]</a>.
+    </li>`,
+    `<li> <b>Characterizing 60 GHz patch antenna segments for fully digital transceiver</b> <br>
+        Jaakko Haarla, Vasilii Semkin, <b>Kai Zheng</b>, Aditya Dhananjay, Marco Mezzavilla, Juha Ala-Laurinaho, Ville Viikari. <br>
+        14th European Conference on Antennas and Propagation (<b>EuCAP</b>). 2020. <br>
+        <a href="/paper/haarla2020characterizing.pdf">[PDF]</a>.
+    </li>`,
+    `<li> <b>Software-defined Radios to Accelerate mmWave Wireless Innovation</b><br>
+        <b>Kai Zheng</b>, Aditya Dhananjay, Marco Mezzavilla, Arjuna Madanayake, et al. <br>
+        The IEEE International Symposium on Dynamic Spectrum Access Networks (<b>DySPAN</b>). 2019.<br>
+        <a href="/paper/dyspan19-sdr.pdf">[PDF]</a>.
+    </li>`,
+    `<li> <b>MillimeTera: Toward a large-scale open-source mmWave and terahertz experimental testbed.</b><br>
+        Polese, Michele, Francesco Restuccia, Abhimanyu Gosain, Josep Jornet, Shubhendu Bhardwaj, Viduneth Ariyarathna, Soumyajit Mandal, <b>Kai Zheng</b>, et al. <br>
+        Proceedings of the 3rd ACM Workshop on Millimeter-wave Networks and Sensing Systems (<b>mmNets</b>). 2019. <br>
+        <a href="/paper/polese2019millimetera.pdf">[PDF]</a>.
+    </li>`,
+];
